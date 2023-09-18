5ce01199a7e62ce39baf5f1ce213c10f8469e129
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2023-09-18 18:55:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://e-comm-web-two.vercel.app');
   next();
 });
-console.log("Hello");
 
 const corsOptions = {
   origin: 'https://e-comm-web-two.vercel.app',
