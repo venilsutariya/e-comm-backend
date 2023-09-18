f7e0431f06a8b82aa0569adf27e8d553ddf8f59c
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2023-09-18 20:25:52 +0530

    Update allowCors.js

--- config/allowCors.js
+++ config/allowCors.js
@@ -1,4 +1,5 @@
-export const allowCors = (req, res, next) => {
+const allowCors = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://e-comm-web-mu.vercel.app/"); // Allow requests from any origin
   next();
 };
+module.exports = allowCors;
